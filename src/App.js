f5d11a15d6f9e0045747198a039ba5e3a4df983f
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-19 17:54:08 -0300

    Buttons para testar a função de idioma

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function sessionTest(valid){
+    if(valid === 'start'){
+      window.sessionStorage.setItem("session", 'true')
+      window.sessionStorage.setItem("lang", 'pt')
+      window.location.reload()
+    }else{
+      window.sessionStorage.clear()
+      window.location.reload()
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button onClick={()=> sessionTest("start")}>Session test start</button>
+       <button onClick={()=> sessionTest('stop')}>Session test stop</button>
       </header>
     </div>
   );
