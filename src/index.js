3410a5370893609e351f259307030600be298dcb
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-19 17:53:52 -0300

    Adicionando index.js com a função de detectar idioma do usuário

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import './index.css';
+  import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  var pathname = window.location.pathname
+  var lang = 'en'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  if(window.localStorage.getItem('lang') === null){ // Verifica se este dispositivo já tem registro de idioma no localStorage! caso não tenha ele cria um conforme os idiomas preferidos do navegador dele.
+    console.log("Primeira vez entrando por este dispositivo!")
+    console.log(`Idioma preferido de seu dispositivo: ${window.navigator.language}`)
+    var prefLang = window.navigator.language
+    switch (prefLang){
+      case 'pt-BR':
+        console.log('idioma do site definido como: pt-br')
+        lang = 'pt-br'
+      break;
+      case 'pt':
+        console.log('idioma do site definido como: pt')
+        lang = 'pt'
+      break;
+      case 'en':
+        console.log('language defined as: en')
+        lang = 'en'
+      break;
+      case 'en-US':
+        lang = 'en-us'
+        console.log('language defined as: en-us')
+      break;
+      default:
+        console.log("We didn't find your preferred language in our language database :c so we are setting the site's language to default English")
+        lang = 'en'
+      break;
+    }
+
+    window.localStorage.setItem('lang', lang)
+
+  }else { // se já tem ele avisa no console.log o idioma selecionado e define a variavel 'lang' para o idioma do localStorage.
+    if(window.sessionStorage.getItem('session') !== 'true'){
+      console.log(`Language selects by LocalStorage was: ${window.localStorage.getItem('lang')}, As soon as you log in to your account, the site will search for the account's defined language!`)
+      lang = window.localStorage.getItem("lang")
+      console.log(lang)
+    }
+  }
+  pathname = pathname.slice(1, pathname.length)
+  pathname = pathname.slice(0, pathname.indexOf('/'))
+  
+  if(pathname === 'pt-br'){
+    console.log("Portugues brasil")
+    langValicacao(lang, 'pt-br')
+  }else if(pathname === 'pt'){
+    console.log('Portugues')
+    langValicacao(lang, 'pt')
+  }else if(pathname === 'en'){
+    console.log('Ingles')
+    langValicacao(lang, 'en')
+  }else{
+    console.log(pathname)
+    langValicacao(lang, pathname)
+  }
+
+
+  function langValicacao(langV, pathname){
+
+    if(window.sessionStorage.getItem("session") === 'true'){
+
+      if(pathname !== window.sessionStorage.getItem("lang")){
+        var newLocSession = window.location.pathname
+        newLocSession = newLocSession.replace(`/${pathname}/`, `/${window.sessionStorage.getItem("lang")}/`)
+        window.location = newLocSession
+      }
+      return 
+    }
+    if(langV !== pathname){
+      console.log('Idiomas diferentes encontrados! realocando para o idioma de localstorage!')
+      console.log(window.location.pathname)
+      var newLoc = window.location.pathname
+      newLoc = newLoc.replace(`/${pathname}/`, `/${window.localStorage.getItem('lang')}/`)
+      window.location = newLoc
+      
+    }else{
+      console.log('Idioma Validado!')
+    }
+  }
+
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
