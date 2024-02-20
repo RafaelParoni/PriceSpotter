a9f40f6890a0d8273abc4d0da805ac87a2f7b0d7
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-20 16:16:08 -0300

    Removendo Test de login em App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 
 function App() {
-  function sessionTest(valid){
-    if(valid === 'start'){
-      window.sessionStorage.setItem("session", 'true')
-      window.sessionStorage.setItem("lang", 'pt')
-      window.location.reload()
-    }else{
-      window.sessionStorage.clear()
-      window.location.reload()
-    }
-  }
+
   return (
     <div className="App">
       <header className="App-header">
-       <button onClick={()=> sessionTest("start")}>Session test start</button>
-       <button onClick={()=> sessionTest('stop')}>Session test stop</button>
+       
       </header>
     </div>
   );
