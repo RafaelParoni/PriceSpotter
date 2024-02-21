c08d821d2ad8b1cb827ba0e82496907ebc4375ea
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-21 17:58:07 -0300

    Adicionando Alt em <img NotFoundImg />

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -8,7 +8,7 @@ function NotFoundPage() {
   return (
     <div className="Notfound">
       <h2>Error</h2>
-      <img src={NotFoundImg} />
+      <img alt='IMAGE ERROR EXEMPLE' src={NotFoundImg} />
       <h2>Pagina não encontrada</h2>
       <button onClick={()=> window.location = '/'}>Voltar ao inicio</button>
     </div>
