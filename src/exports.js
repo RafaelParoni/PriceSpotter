f9979d0543df805677f94088dea0ffaba2bc4414
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-21 23:19:57 -0300

    Adicionando Arquivo exports.js para criar variáveis de exportação e adicionando Languages a este arquivo

---
+++ src/exports.js
@@ -0,0 +1,22 @@
+var Languages = { // Traduções de paginas para EN, PT e ES
+    "en": {
+        "NotFound": {
+            "Title": "Page not found",
+            "Button": 'Back to start',
+        },
+    },
+    "pt": {
+        "NotFound": {
+            "Title": "Pagina não encontrada",
+            "Button": 'Voltar ao início',
+        },
+    },
+    'es': {
+        "NotFound": {
+            "Title": "Página no encontrada",
+            "Button": 'De regreso al comienzo',
+        },
+    },
+}
+
+export {Languages} // Exportando Languages
