5f8104d5b793c8ae000c96f7d33a66a241d36b0c
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-21 23:26:19 -0300

    Adicionando Sistema para adicionar tradução para a pagina NotFoundPage

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,15 +2,31 @@ import './NotFound.css';
 
 import NotFoundImg from './../../Imgs/Error404.png'
 
+import { Languages } from '../../exports';
 
 function NotFoundPage() {
+
+  var langL = window.localStorage.getItem('lang')
+  var langA = {}
+
+  switch(langL){
+    case "pt":
+      langA = Languages.pt.NotFound
+    break;
+    case "es":
+      langA = Languages.es.NotFound
+    break;
+    default: 
+      langA = Languages.en.NotFound
+    break;
+  }
 
   return (
     <div className="Notfound">
       <h2>Error</h2>
       <img alt='IMG ERROR EXEMPLE' src={NotFoundImg} />
-      <h2>Pagina não encontrada</h2>
-      <button onClick={()=> window.location = '/'}>Voltar ao inicio</button>
+      <h2>{langA.Title}</h2>
+      <button onClick={()=> window.location = '/'}>{langA.Button}</button>
     </div>
   );
 }
