678879b702f52fc7a29e3a0334e13baa927e6a69
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-22 00:27:54 -0300

    Adicionando Formato Básico da função de pesquisar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import './Navbar.css';
 
 import logo356 from './../../Imgs/Logo 356x.png'
 
+import { LuSearch } from "react-icons/lu";
+
 function NavbarComp() {
 
 
@@ -12,7 +14,14 @@ function NavbarComp() {
             <img src={logo356} height={50} alt="logos" />
             <h2>Price Spotter</h2>
         </div>
-        <div className='navbar-search'></div>
+        <div className='navbar-search'>
+            <div className='search'>
+                <LuSearch color='000'/>
+                <input id='search' results={5} type='text' />
+                <button id='clerSearch' onClick={()=> document.getElementById('search').value = ''}>X</button>
+                <button id='StartSearch' onClick={()=> document.getElementById('search').value = ''}><LuSearch color='000'/></button>
+            </div>
+        </div>
         <div className='navbar-extra'></div>
     </div>
   );
