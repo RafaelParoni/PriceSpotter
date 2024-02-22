87f6312fea9c7ed3e4031ee068a2c029f9c9066e
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-22 17:09:34 -0300

    Finalizando a parte de pesquisa da NavbarComp

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,9 +3,41 @@ import './Navbar.css';
 
 import logo356 from './../../Imgs/Logo 356x.png'
 
-import { LuSearch } from "react-icons/lu";
+import { LuSearch, LuEraser  } from "react-icons/lu";
+import { useState } from 'react';
 
 function NavbarComp() {
+
+
+  var [SearchValue, setSearchValue] = useState('')
+
+  const updataSearchValue = (value, lenght) => {
+    setSearchValue(value)
+    if(lenght > 0){
+      document.getElementById("ClearSearchBtn").style.visibility = 'visible'
+    }else{
+      document.getElementById("ClearSearchBtn").style.visibility = 'hidden'
+    }
+  }
+
+  function startSearch(){
+    if(SearchValue.length !== 0){
+      SearchValue = SearchValue.trim()
+      if(SearchValue.includes(" ")){
+        var i = 0
+        while(i < 5){
+          SearchValue = SearchValue.replace(' ', '-')
+
+          if(!SearchValue.includes(" ")){
+            i++
+          }
+        }
+  
+      }
+      console.log(`pesquisando: ${SearchValue}`)
+      window.location = `/${window.localStorage.getItem("lang")}/search/${SearchValue}`
+    }
+  }
 
 
   return (
@@ -16,10 +48,19 @@ function NavbarComp() {
         </div>
         <div className='navbar-search'>
             <div className='search'>
-                <LuSearch color='000'/>
-                <input id='search' results={5} type='text' />
-                <button id='clerSearch' onClick={()=> document.getElementById('search').value = ''}>X</button>
-                <button id='StartSearch' onClick={()=> document.getElementById('search').value = ''}><LuSearch color='000'/></button>
+                <LuSearch size={15} color='000'/>
+                <input 
+                  id='search' 
+
+                  value={SearchValue} 
+                  onChange={(e)=> updataSearchValue(e.target.value, e.target.value.length)} 
+                  placeholder='Pesquisar jogo' 
+                  results={5} 
+                  type='search' 
+                  onKeyDown={event => {if (event.key === 'Enter') {startSearch()}}} 
+                />
+                <button id='ClearSearchBtn' onClick={()=> updataSearchValue('', 0)}><LuEraser size={15} color='fff'/></button>
+                <button id='StartSeachBtn' onClick={()=> startSearch()}><LuSearch size={15} color='fff'/></button>
             </div>
         </div>
         <div className='navbar-extra'></div>
