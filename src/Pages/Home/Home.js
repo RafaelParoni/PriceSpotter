cb19a8d060cae8d73d8c8cc9911dad4b2a4bedc7
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-23 02:27:16 -0300

    Adicionando a busca das apis e HomePage e atualizando css base

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,19 +1,50 @@
 import './Home.css';
 
+import { useState } from 'react';
+
 import { freeGamelist } from '../../Components/Epic Games/freeGameList';
+import { freeGameComingSoonList } from '../../Components/Epic Games/freeGameComingSoon';
 
 
 
 function HomePage() {
 
-  freeGamelist().then(function(result){
-    console.log(result)
-  })
+
+  var [freeGame, setFreeGame] = useState({})
+  var [freeGameComing, setFreeGameComing] = useState({})
+
+  const SearchFreeGame = () => {
+    if(Object.keys(freeGame).length > 0){return}else{
+      freeGamelist().then(function(result){
+        setFreeGame(result)
+      })
+    }
+  }
+  SearchFreeGame()
+  const SearchFreeGameComing = () => {
+    if(Object.keys(freeGameComing).length > 0){return}else{
+      freeGameComingSoonList().then(function(result){
+        setFreeGameComing(result)
+      })
+    }
+  }
+  SearchFreeGameComing()
+
+
 
   return (
     <>
       <div className="Home">
-        
+        <div className='home-epic-free'>
+          <button onClick={()=> {console.log(freeGame); console.log(freeGameComing)}}>teste</button>
+
+        </div>
+        <div className='home-news'>
+
+        </div>
+        <div>
+
+        </div>
       </div>
     </>
   );
