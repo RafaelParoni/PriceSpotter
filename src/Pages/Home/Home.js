c0b557f33cfca58df8e319dfccdd1dfe9e1e3bcb
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-23 14:58:44 -0300

    Finalizando HomePage/EpicGamesFreeGames

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,14 +4,15 @@ import { useState } from 'react';
 
 import { freeGamelist } from '../../Components/Epic Games/freeGameList';
 import { freeGameComingSoonList } from '../../Components/Epic Games/freeGameComingSoon';
+import { translateAPI } from '../../Components/Translate/translate';
 
-
+import { LuGift } from 'react-icons/lu'
 
 function HomePage() {
 
 
-  var [freeGame, setFreeGame] = useState({})
-  var [freeGameComing, setFreeGameComing] = useState({})
+  var [freeGame, setFreeGame] = useState([])
+  var [freeGameComing, setFreeGameComing] = useState([])
 
   const SearchFreeGame = () => {
     if(Object.keys(freeGame).length > 0){return}else{
@@ -32,12 +33,56 @@ function HomePage() {
 
 
 
+
+  var [descirption01, setDescription01] = useState('')
+  function FreeGameDisplay({item}){
+      translateAPI(item.description, 'pt').then(function(result){
+        if(result === 'error'){
+          setDescription01(item.description)
+        }else{
+          setDescription01(result)
+        }
+      })
+    return (
+        <div onClick={()=> window.open(item.appUrl)}> 
+          <img alt='Game free - store.epicgames.com' src={item.offerImageWide} />
+          <h4>FREE NOW</h4>
+          <h2>{item.name}</h2> 
+          <h5>{descirption01}</h5> 
+        </div>
+    )
+  }
+  var [descirption02, setDescription02] = useState('')
+  function FreeGameComingDisplay({item}){
+    translateAPI(item.description, 'pt').then(function(result){
+      if(result === 'error'){
+        setDescription02(item.description)
+      }else{
+        setDescription02(result)
+      }
+    })
+    return (
+      <div onClick={()=> window.open(item.appUrl)}> 
+        <img alt='Game free coming soon - store.epicgames.com' src={item.offerImageTall} /> 
+        <h4>COMING SOON</h4>
+        <h2>{item.name}</h2> 
+        <h5>{descirption02} </h5> 
+      </div>
+    )
+  }
+  
+
+
+
   return (
     <>
       <div className="Home">
         <div className='home-epic-free'>
-          <button onClick={()=> {console.log(freeGame); console.log(freeGameComing)}}>teste</button>
-
+          <h2><LuGift/> Epic Games Free</h2>
+          <div className='epic-cards'>
+            {freeGame.map((freeGame) => <FreeGameDisplay key={freeGame.name} item={freeGame} />)}
+            {freeGameComing.map((freeGameComing) => <FreeGameComingDisplay key={freeGameComing.name} item={freeGameComing} />)}
+          </div>
         </div>
         <div className='home-news'>
 
