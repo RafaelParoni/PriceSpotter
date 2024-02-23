9b99ec2c94679067242b4c79c8b612d6f71e5708
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-23 14:41:10 -0300

    Adicionando Api de tradução para traduzir automaticamente textos não traduzidos de APIs

---
+++ src/Components/Translate/translate.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+export async function translateAPI(text, to){
+    const options = {
+      method: 'POST',
+      url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+      params: {
+        'to[0]': to,
+        'api-version': '3.0',
+        profanityAction: 'NoAction',
+        textType: 'plain'
+      },
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '4d1fc03470msh98ed2d469a33f37p102184jsn7cab8e913b66',
+        'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+      },
+      data: [
+        {
+          Text: text
+        }
+      ]
+    };
+
+    try {
+      const response = await axios.request(options);
+        console.log(response)
+    } catch (error) {
+      console.error(error);
+    }
+}
