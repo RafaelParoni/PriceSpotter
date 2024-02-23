11916c2b7101733dd22714152580fcfa09e96a0d
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-23 20:58:50 -0300

    Criando o aruivos NewsLists.js e .css

---
+++ src/Pages/News/NewsList/NewsList.js
@@ -0,0 +1,14 @@
+import './NewsList.css';
+
+function NewsListPage() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>NEW LIST</h1>
+      </header>
+    </div>
+  );
+}
+
+export default NewsListPage;
