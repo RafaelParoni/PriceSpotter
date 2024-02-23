a5bc42063a3b9c782ab10104226b5de2b4c6e3f3
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-23 00:50:37 -0300

    Finalizando NavbarComp

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 import logo356 from './../../Imgs/Logo 356x.png'
 
-import { LuSearch, LuEraser  } from "react-icons/lu";
+import { LuSearch, LuEraser,LuMenu, LuX } from "react-icons/lu";
 import { useState } from 'react';
 
 function NavbarComp() {
@@ -39,32 +39,57 @@ function NavbarComp() {
     }
   }
 
+  function extraContent(){
+    if(document.getElementById('CloseExtraBtn').style.display === 'none'){
+        document.getElementById("CloseExtraBtn").style.display = 'flex'
+        document.getElementById('OpenExtraBtn').style.display = 'none'
+        document.getElementById("extraContent").style.display = 'flex'
+        document.getElementById("extraContentClose").style.display = 'flex'
+    }else{
+      document.getElementById("CloseExtraBtn").style.display = 'none'
+        document.getElementById('OpenExtraBtn').style.display = 'flex'
+        document.getElementById("extraContent").style.display = 'none'
+        document.getElementById("extraContentClose").style.display = 'none'
+    }
+  }
 
   return (
-    <div className="Navbar">
-        <div className='navbar-logo' onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/`}>
-            <img src={logo356} height={50} alt="logos" />
-            <h2>Price Spotter</h2>
-        </div>
-        <div className='navbar-search'>
-            <div className='search'>
-                <LuSearch size={15} color='000'/>
-                <input 
-                  id='search' 
+    <>
+      <div className="Navbar">
+          <div className='navbar-logo' onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/`}>
+              <img src={logo356} height={50} alt="logos" />
+              <h2>Price Spotter</h2>
+          </div>
+          <div className='navbar-search'>
+              <div className='search'>
+                  <LuSearch size={15} color='000'/>
+                  <input 
+                    id='search' 
 
-                  value={SearchValue} 
-                  onChange={(e)=> updataSearchValue(e.target.value, e.target.value.length)} 
-                  placeholder='Pesquisar jogo' 
-                  results={5} 
-                  type='search' 
-                  onKeyDown={event => {if (event.key === 'Enter') {startSearch()}}} 
-                />
-                <button id='ClearSearchBtn' onClick={()=> updataSearchValue('', 0)}><LuEraser size={15} color='fff'/></button>
-                <button id='StartSeachBtn' onClick={()=> startSearch()}><LuSearch size={15} color='fff'/></button>
-            </div>
-        </div>
-        <div className='navbar-extra'></div>
-    </div>
+                    value={SearchValue} 
+                    onChange={(e)=> updataSearchValue(e.target.value, e.target.value.length)} 
+                    placeholder='Pesquisar jogo' 
+                    results={5} 
+                    type='search' 
+                    onKeyDown={event => {if (event.key === 'Enter') {startSearch()}}} 
+                  />
+                  <button id='ClearSearchBtn' onClick={()=> updataSearchValue('', 0)}><LuEraser size={15} color='fff'/></button>
+                  <button id='StartSeachBtn' onClick={()=> startSearch()}><LuSearch size={15} color='fff'/></button>
+              </div>
+          </div>
+          <div className='navbar-extra'>
+            <button id='CloseExtraBtn'  onClick={extraContent} style={{display: 'none'}}><LuX  color='000' size={25}/></button>
+            <button id='OpenExtraBtn' onClick={extraContent} style={{display: 'flex'}}><LuMenu  color='000' size={25}/></button>
+          </div>
+      </div>
+      <div onClick={extraContent} id='extraContentClose'></div>
+      <div id='extraContent' className='extra-content'>
+        <h3 onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/`}> <img src={logo356} width={25} alt="logos" /> Price Spotter Menu</h3>
+        <button className='extra-options' onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/`}>Inicio</button>
+        <button className='extra-options' onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/history/`}>Historico</button>
+        <button className='extra-options' onClick={()=> window.location = `/${window.localStorage.getItem('lang')}/settings/`}>Configuração do site</button>
+      </div>
+    </>
   );
 }
 
