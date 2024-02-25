6817a04cab9a5157f4cdf53cfb0e39f74a31e28b
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-02-25 18:34:37 -0300

    Adicionando metade da pagina game/details/ (Não finalizada)

--- src/Pages/Game/Game.js
+++ src/Pages/Game/Game.js
@@ -1,8 +1,8 @@
 import './Game.css';
 
-import { LuCherry } from "react-icons/lu";
+import { LuCherry, LuX, LuChevronLeft, LuChevronRight     } from "react-icons/lu";
 
-import { useState } from 'react';
+import { useState, } from 'react';
 
 import { EpicGamesStoreApi } from '../../Components/Epic Games/epicStoreApi';
 
@@ -31,41 +31,121 @@ function GamePage() {
 
   var [epicResults, setEpicResults] = useState([])
   var [epicResultsStats, setEpicResultsStats] = useState('loading')
+  var [slideImgs, setSlideImgs] = useState([])
 
-  if(detailsType === 'epic'){
-    var CoinSelect = window.localStorage.getItem("coin")
-    console.log(CoinSelect)
-    function getEpicGames(){
+
+  function EpicGamesDisplay({item}){
+    return (
+      <h1>nome</h1>
+    )
+  }
+
+  var CoinSelect = window.localStorage.getItem("coin")
+  function getEpicGames(){
       if(Object.keys(epicResults).length > 0){return}else{
         EpicGamesStoreApi(detailsValue, CoinSelect).then(function(result){
           if(result.code){
             setEpicResultsStats('error')
           }else{
             setEpicResults(result[0])
-            console.log(result[0])
             if(result["error-index"] === "1"){
               setEpicResultsStats('notFound')
             }else{
               setEpicResultsStats('sucess')
+              setSlideImgs(result[0].keyImages) 
+
             }
           }
         })
       }
+  }
+
+
+  var Slide = 0
+
+  function SlideViewImgs(type){
+    if(type === 'start'){
+      document.getElementById(slideImgs[Slide].type).style.display = 'none'
+      document.getElementById(slideImgs[0].type).style.display = 'flex'
+      Slide = 0
+      document.getElementById('SlideValue').value = Slide
+    }else if(type === 'next'){
+      if(Slide === slideImgs.length -1){
+        console.log(`Slide final: ${Slide}, Voltando para Slide: 0`)
+        document.getElementById(slideImgs[Slide].type).style.display = 'none'
+        Slide = 0
+        document.getElementById(slideImgs[Slide].type).style.display = 'flex'
+      }else{
+        console.log(`Slide anterior: ${Slide} - Proximo Slide: ${Slide + 1}`)
+        console.log('ID: ' + slideImgs[Slide].type)
+        document.getElementById(slideImgs[Slide].type).style.display = 'none'
+        Slide = Slide + 1
+        document.getElementById(slideImgs[Slide].type).style.display = 'flex'
+      }
+    }else if(type === 'back'){
+     if(Slide === 0){
+      document.getElementById(slideImgs[Slide].type).style.display = 'none'
+      console.log('Chegamos no INICIO dos slides, Voltando para o final! ' + slideImgs.length)
+      Slide = slideImgs.length -1 
+      document.getElementById(slideImgs[Slide].type).style.display = 'flex'
+     }else{
+      console.log(`Slide anterior: ${Slide}, Proximo Slide: ${Slide -1}`)
+      document.getElementById(slideImgs[Slide].type).style.display = 'none'
+      Slide = Slide -1
+      document.getElementById(slideImgs[Slide].type).style.display = 'flex'
+     }
     }
+    document.getElementById('SlideValue').innerHTML = Slide + 1   
+  }
+
+
+  function SlideViewImgsDisplay({item}){
+    return (
+      <img id={item.type} style={{display: 'none'}} src={item.url} />
+    )
+  }
+
+  if(detailsType === 'epic'){
     getEpicGames()
 
   }else{
     
   }
   return (
-    <div className="gamePage">
-      <div className='game-info'>
-          <h2> <LuCherry/> {detailsValue}</h2>
+    <>
+      <div className="gamePage">
+        <div className='game-info'>
+            <h2> <LuCherry/> {detailsValue}</h2>
+        </div>
+        <div className='game-details'>
+          {epicResultsStats === 'sucess' && (
+            <>
+              <div onClick={()=> {document.getElementById('view-img').style.display = 'flex'; document.getElementById('view-img').style.filter = ' opacity(1)'; SlideViewImgs('start')}} className='game-details-img'>
+                <img src={epicResults.keyImages[0].url} className='game-details-img' />
+                <div className='game-details-img-text'>Mais outras {epicResults.keyImages.length -1 } imagens</div>
+              </div>
+              
+              <div className='game-details-info'>
+                <p>{epicResults.description}</p>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className='game-details'>
-        {epicResults.title}
-      </div>
-    </div>
+      {epicResultsStats === 'sucess' && (
+        <div id='view-img' className='games-img-views'>
+            <div className='view-imgs-display'>
+              <button onClick={()=> SlideViewImgs('back')}><LuChevronLeft/></button>
+                {slideImgs.map((slideImgs) => <SlideViewImgsDisplay item={slideImgs} /> )}
+              <button onClick={()=> SlideViewImgs('next')}><LuChevronRight/></button>
+            </div>
+            <div className='view-imgs-control'>
+              <p><p id='SlideValue'>1</p>/{Object.keys(slideImgs).length }</p>
+              <button onClick={()=> {document.getElementById('view-img').style.display = 'none'; document.getElementById('view-img').style.filter = ' opacity(0)'; document.getElementById(slideImgs[Slide].type).style.display = 'none'}}><LuX size={20}/> Fechar</button>
+            </div>
+        </div>
+      )}
+    </>
   );
 }
 
